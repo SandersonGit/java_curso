eb5c60bd88ab095f9daa68fbb9fdfe5e414a71ab
sande <sandersonluiz1988@gmail.com>
2024-02-23 16:15:01 -0300

    primeiro commit

---
+++ variáveis/src/variáveis/variaveis.java
@@ -0,0 +1,11 @@
+package variáveis;
+
+public class variaveis {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		int result = a * b;
+				
+		System.out.println("A multiplicação dos numeros é: " + result);
+	}
+}
