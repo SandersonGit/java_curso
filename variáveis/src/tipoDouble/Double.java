0d5fdd5b3acd898c3a473e0044a1608ba06e6ad1
sande <sandersonluiz1988@gmail.com>
2024-02-23 16:58:20 -0300

    frist commit

---
+++ variáveis/src/tipoDouble/Double.java
@@ -0,0 +1,19 @@
+package tipoDouble;
+
+public class Double {
+
+	public static void main(String[] args) {
+		double hora = 3;
+		System.out.println(hora);
+		
+		double conta = 3.5 / 2;
+		System.out.println(conta);
+
+		int conta2 = 3 / 2;
+		System.out.println(conta2);
+		
+		double conta3 = 3 / 2;
+		System.out.println(conta3);
+	}
+
+}
