36ba8357c90b6b6a2335d56362eab47500a702d7
sande <sandersonluiz1988@gmail.com>
2024-02-23 17:43:44 -0300

    commit

---
+++ variáveis/src/cast/Cast.java
@@ -0,0 +1,12 @@
+package cast;
+
+public class Cast {
+
+	public static void main(String[] args) {
+		double salario = 2700.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+
+	}
+
+}
